c11a9e7d92cffbe75520ade2cc3f3a22b976e209
eliranRP <eliranazulay@gmail.com>
2018-12-21 11:14:02 +0200

    Update Router.js

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginContainer from '../src/Features/Authentication/screens/LoginContainer'
-import { ImageSlider } from './Features/Feed/components/ImageSlider';
 
 
 const RouterComponent = () => {
